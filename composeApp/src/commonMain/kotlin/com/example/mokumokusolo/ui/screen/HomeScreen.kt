d6a527a2fa6b04836f97cc2f1173f8e15bb8005b
rintarotajima <rintarotajima@gmail.com>
2025-08-27 11:58:49 +0900

    implement the app list

--- composeApp/src/commonMain/kotlin/com/example/mokumokusolo/ui/screen/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/mokumokusolo/ui/screen/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -17,10 +18,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.mokumokusolo.model.App
+import mokumokusolo.composeapp.generated.resources.Res
+import mokumokusolo.composeapp.generated.resources.duolingo
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val sampleApps = listOf(
+        App(
+            id = 1,
+            name = "Duolingo",
+            image = painterResource(Res.drawable.duolingo),
+            revenue = 1000.0
+        ),
+        App(
+            id = 2,
+            name = "Duolingo2",
+            image = painterResource(Res.drawable.duolingo),
+            revenue = 1500.0
+        ),
+        App(
+            id = 3,
+            name = "Duolingo3",
+            image = painterResource(Res.drawable.duolingo),
+            revenue = 2000.0
+        )
+    )
     Scaffold(
         modifier = modifier
             .fillMaxSize()
@@ -50,6 +75,10 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 AppsChip()
                 ExpenditureChip()
             }
+            AppList(
+                appList = sampleApps,
+                modifier = Modifier.width(300.dp)
+            )
         }
     }
 }
