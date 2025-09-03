d107088bba02b290296d57abc5463e89555c2503
rintarotajima <rintarotajima@gmail.com>
2025-09-03 21:14:21 +0900

    Implementation of screen transitions when pressing the FAB button

--- composeApp/src/commonMain/kotlin/com/example/mokumokusolo/ui/screen/AddItemScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/mokumokusolo/ui/screen/AddItemScreen.kt
@@ -1,30 +1,38 @@
 package com.example.mokumokusolo.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddItemScreen(modifier: Modifier = Modifier) {
+fun AddItemScreen(
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
             CenterAlignedTopAppBar(
                 title = { Text(text = "追加") },
                 navigationIcon = {
-                    IconButton(onClick = {}) {
+                    IconButton(onClick = onClose) {
                         Icon(
                             imageVector = Icons.Default.Close,
                             contentDescription = "Close"
@@ -35,8 +43,27 @@ fun AddItemScreen(modifier: Modifier = Modifier) {
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "アプリ名") },
+                placeholder = { Text(text = "MokuMokuSolo") }
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "現在の収益(月)") },
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(onClick = {}) {
+                Text("登録")
+            }
         }
     }
 }
@@ -44,5 +71,5 @@ fun AddItemScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun AddItemScreenPreview() {
-    AddItemScreen()
+    AddItemScreen(onClose = {})
 }
