2046ccbec6baa61c9467ba4e9253496dd9921665
rintarotajima <rintarotajima@gmail.com>
2025-10-14 14:43:55 +0900

    feat: Daoの定義（挿入処理の修正)

--- composeApp/src/commonMain/kotlin/com/example/mokumokusolo/data/database/dao/AppDao.kt
+++ composeApp/src/commonMain/kotlin/com/example/mokumokusolo/data/database/dao/AppDao.kt
@@ -3,13 +3,14 @@ package com.example.mokumokusolo.data.database.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.mokumokusolo.data.database.entity.App
 
 @Dao
 interface AppDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(app: App)
 
     @Update
--- composeApp/src/commonMain/kotlin/com/example/mokumokusolo/data/database/dao/ExpenditureDao.kt
+++ composeApp/src/commonMain/kotlin/com/example/mokumokusolo/data/database/dao/ExpenditureDao.kt
@@ -3,13 +3,14 @@ package com.example.mokumokusolo.data.database.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.mokumokusolo.data.database.entity.Expenditure
 
 @Dao
 interface ExpenditureDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(expenditure: Expenditure)
 
     @Update
