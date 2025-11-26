5c406cec98bee53ba2f5a812180abb5c60196a62
rintarotajima <rintarotajima@gmail.com>
2025-11-26 18:38:48 +0900

    App Bundleのバージョン変更(1.0.2→1.0.3)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -71,8 +71,8 @@ android {
         applicationId = "com.rintaroo.mokumokusolo"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
     }
     packaging {
         resources {
